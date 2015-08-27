f2f2c57705fc27c80ed9b2489f87cce606d63299
Carlos Eduardo Alves <kmiksi@github>
2015-08-27 04:38:05 -0300

    routing fixes

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
 define('CORE', ROOT . 'core' . DIRECTORY_SEPARATOR);
-define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . DIRECTORY_SEPARATOR);
+define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . '/');
 
-require ROOT . 'core/FrontController.php';
+require ROOT . 'core' . DIRECTORY_SEPARATOR . 'FrontController.php';
 
 $app = new FrontController();
