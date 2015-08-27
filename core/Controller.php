95fa3718beb8d25a355ca96f7e3b4a0e1f558ef8
Carlos Eduardo Alves <kmiksi@github>
2015-08-27 02:33:03 -0300

    controller adjustments

--- core/Controller.php
+++ core/Controller.php
@@ -8,16 +8,16 @@ class Controller {
 
     public function render($view, $layout = 'default') {
         if ($this->render) { // prevent rendering the same action twice
-            $this->view('layout/' . $layout . '_start.php');
+            $this->view('layout/' . $layout . '_start');
             $this->view($view);
-            $this->view('layout/' . $layout . '_end.php');
+            $this->view('layout/' . $layout . '_end');
 
             $this->render = false; // already rendered
         }
     }
 
     public function view($path) {
-        require APP . 'View/' . $path;
+        require APP . 'View/' . $path . '/.php';
     }
 
 }
