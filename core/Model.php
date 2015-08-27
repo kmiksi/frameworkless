d8b610bc36e4b97cb6a2686a5705d8a910480138
Carlos Eduardo Alves <kmiksi@github>
2015-08-27 04:27:13 -0300

    Model improvements and documentation

--- core/Model.php
+++ core/Model.php
@@ -2,8 +2,12 @@
 
 class Model {
 
+    /** @var PDO PDO connection */
     static $db = null;
 
+    /**
+     * Set a PDO connection to be reused
+     */
     private static function setDataSourse() {
         try {
             $datasoursename = DBConfig::TYPE . ':host=' . DBConfig::HOST . ';dbname=' . DBConfig::NAME;
@@ -12,19 +16,27 @@ class Model {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
             );
 
-            static::$db = new PDO($datasoursename, DBConfig::USER, DBConfig::PASS, $options);
+            self::$db = new PDO($datasoursename, DBConfig::USER, DBConfig::PASS, $options);
         } catch (PDOException $e) {
             exit('Database connection could not be established.');
         }
     }
 
+    /**
+     * @return PDO PDO connection
+     */
     public function db() {
-        if (empty(static::$db)) {
-            static::setDataSourse();
+        if (empty(self::$db)) {
+            self::setDataSourse();
         }
-        return static::$db;
+        return self::$db;
     }
 
+    /**
+     * @param string $sql
+     * @param array $named_params
+     * @return Model Object representation of query
+     */
     public function query($sql, $named_params = array()) {
         $query = $this->db()->prepare($sql);
         $query->execute($named_params);
