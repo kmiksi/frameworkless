d54762fd88454a9a5b6de6346064adda8b003d04
Carlos Eduardo Alves <kmiksi@github>
2015-08-27 07:45:57 -0300

    basic layout done

---
+++ app/View/layout/default_start.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : Undeviating 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140322
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title></title>
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+        <link href="<?= BASE_URL ?>css/default.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="<?= BASE_URL ?>css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+        <script>var BASE_URL = "<?= BASE_URL ?>";</script>
+    </head>
+    <body>
+        <div id="header-wrapper">
+            <div id="header" class="container">
+                <div id="logo">
+                    <span class="icon icon-cog"></span>
+                    <h1><a href="#">Frameworkless TODO-list app</a></h1>
+                </div>
+                <div id="menu">
+                    <ul>
+                        <li><a href="http://turnkey-lapp/todo/frameworkless/public/home" accesskey="1" title="">Homepage</a></li>
+                        <li><a href="http://turnkey-lapp/todo/frameworkless/public/todo" accesskey="2" title="">TODOs</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div id="page-content" class="container">
