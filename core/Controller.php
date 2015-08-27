e990b5b9714064a4d295796cf0e7c0697e033b2b
Carlos Eduardo Alves <kmiksi@github>
2015-08-27 03:47:29 -0300

    improved view rendering architecture

--- core/Controller.php
+++ core/Controller.php
@@ -2,10 +2,29 @@
 
 class Controller {
 
-    /** @var Model */
-    public $model = null;
+    /** @var bool Control the layout rendering of action */
     public $render = true;
 
+    /** @var array Store variables to be passed to view rendering */
+    public $viewVars = array();
+
+    /**
+     * Store values to be passed to view rendering
+     * @param string|array $name The name of variable, or an array with name=>value (php method compact() can help you)
+     * @param mixed $value
+     */
+    public function set($name, $value = null) {
+        if (!is_array($name)) {
+            $name = array($name => $value);
+        }
+        $this->viewVars = array_merge($this->viewVars, $name);
+    }
+
+    /**
+     * Render full layout of specifyed view (once per controller instance)
+     * @param string $view View name (action name)
+     * @param string $layout Layout name
+     */
     public function render($view, $layout = 'default') {
         if ($this->render) { // prevent rendering the same action twice
             $this->view('layout/' . $layout . '_start');
@@ -16,8 +35,13 @@ class Controller {
         }
     }
 
+    /**
+     * Render a view file
+     * @param string $path Path relative to View folder without extension
+     */
     public function view($path) {
-        require APP . 'View/' . $path . '/.php';
+        extract($this->viewVars);
+        require APP . 'View/' . $path . '.php';
     }
 
 }
--- core/FrontController.php
+++ core/FrontController.php
@@ -9,32 +9,33 @@ class FrontController {
     public function __construct() {
         // filter controller, action and params
         $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL); // $_GET['url']
-        $url_params = explode('/', trim($url, '/'));
+        $params = explode('/', trim($url, '/'));
 
         // store first and seccond params, removing them from params list
-        $url_controller = array_shift($url_params);
-        $url_action = array_shift($url_params);
+        $controller_name = array_shift($params);
+        $action_name = array_shift($params);
 
         require_once APP . 'config.php';
 
-        // default method and action
-        if (empty($url_controller)) {
-            $url_controller = AppConfig::DEFAULT_CONTROLLER;
+        // default controller and action
+        if (empty($controller_name)) {
+            $controller_name = AppConfig::DEFAULT_CONTROLLER;
         }
-        if (empty($url_action)) {
-            $url_action = AppConfig::DEFAULT_ACTION;
+        if (empty($action_name)) {
+            $action_name = AppConfig::DEFAULT_ACTION;
         }
 
         // load requested controller
-        if (file_exists(APP . "Controller/$url_controller.php")) {
+        if (file_exists(APP . "Controller/$controller_name.php")) {
             require CORE . "Controller.php";
             require CORE . "Model.php";
-            require APP . "Controller/$url_controller.php";
-            $controller = new $url_controller();
+            require APP . "Controller/$controller_name.php";
+            $controller = new $controller_name();
 
             // verify if action is valid
-            if (method_exists($controller, $url_action)) {
-                call_user_func_array(array($controller, $url_action), $url_params);
+            if (method_exists($controller, $action_name)) {
+                call_user_func_array(array($controller, $action_name), $params);
+                $controller->render($action_name); // skipped if already rendered
             } else {
                 // action not found
                 $this->notFound();
