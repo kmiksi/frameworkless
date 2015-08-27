db4b4ec1e86a53a8ee8892db3440146137f13e40
Carlos Eduardo Alves <kmiksi@github>
2015-08-27 07:47:06 -0300

    Home controller and view

---
+++ app/View/Home/index.php
@@ -0,0 +1,5 @@
+<div class="title">
+    <h2>Welcome to this TODO-list app!</h2>
+</div>
+<p>Start trying to list TODOs on the "TODOs" tab.</p>
+<p><i>Based on <strong>Undeviating</strong>, a free, fully standards-compliant CSS template. This free template is released under the Creative Commons Attribution license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </i></p>
